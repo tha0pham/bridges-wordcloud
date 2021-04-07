6b308cce9fb9ac52406da469cb75dc48a07eeece
Thao Pham <28838595+thaopham1816@users.noreply.github.com>
2021-04-07 15:36:23 -0400

    Add config info

---
+++ src/edu/uncc/tpham34/Config.java
@@ -0,0 +1,7 @@
+package edu.uncc.tpham34;
+
+public class Config {
+    static final int ASSIGNMENT_NUMBER = // put your assignment number here
+    static final String USERNAME = // put your username here
+    static final String API_KEY = // put your API key here
+}
